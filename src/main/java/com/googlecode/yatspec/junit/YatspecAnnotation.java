e2d6de24d2f2f36844630ca4db5cf6992cce937c
nicholas.mcdowall <nicholas.mcdowall@asos.com>
2019-05-01 08:36:30 +0100

    Refactor class - replace totallylazy methods with vanilla java

--- src/main/java/com/googlecode/yatspec/junit/YatspecAnnotation.java
+++ src/main/java/com/googlecode/yatspec/junit/YatspecAnnotation.java
@@ -1,38 +1,27 @@
 package com.googlecode.yatspec.junit;
 
-import com.googlecode.totallylazy.predicates.LogicalPredicate;
-
-import java.lang.annotation.*;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.Collection;
 import java.util.List;
 
-import static com.googlecode.totallylazy.Predicates.instanceOf;
-import static com.googlecode.totallylazy.Sequences.sequence;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface YatspecAnnotation {
     class methods {
         public static List<Annotation> yatspecAnnotations(Collection<Annotation> annotations) {
-            return sequence(annotations)
-                    .filter(predicates.annotatedWith(YatspecAnnotation.class))
-                    .toList();
-        }
-
-        public static boolean annotatedWith(Class<?> aClass, Class<? extends Annotation> annotation) {
-            return sequence(aClass.getDeclaredAnnotations())
-                    .exists(instanceOf(annotation));
-        }
-    }
-
-    class predicates {
-        public static <T extends Annotation> LogicalPredicate<T> annotatedWith(final Class<? extends Annotation> annotation) {
-            return new LogicalPredicate<T>() {
-                public boolean matches(Annotation instance) {
-                    return methods.annotatedWith(instance.annotationType(), annotation);
-                }
-            };
+            return annotations.stream()
+                    .filter(annotation -> stream(annotation.annotationType().getDeclaredAnnotations())
+                            .anyMatch(declared -> declared.annotationType().equals(YatspecAnnotation.class)))
+                    .collect(toList());
         }
 
     }
+
 }
