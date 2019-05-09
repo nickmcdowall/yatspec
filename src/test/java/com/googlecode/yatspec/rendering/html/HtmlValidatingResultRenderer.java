f89427babd2dc80e85bd15fc170474053a00fc82
nicholas.mcdowall <nicholas.mcdowall@asos.com>
2019-05-09 08:36:08 +0100

    Let the result listener exceptions bubble up and fail the tests (make Travis CI use osx and remove generated comments so that comparing the expected sequence diagram html is easier (plantuml generates different metadata in comments for different variants of the library depending on OS)

--- src/test/java/com/googlecode/yatspec/rendering/html/HtmlValidatingResultRenderer.java
+++ src/test/java/com/googlecode/yatspec/rendering/html/HtmlValidatingResultRenderer.java
@@ -7,10 +7,17 @@ import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Currently validation of HTML only enabled on OSX (includes the Travis CI build)
+ * due to subtle differences in the generated output when using different OSs which
+ * make it a nightmare to test the expectation.
+ */
 public class HtmlValidatingResultRenderer extends HtmlResultRenderer {
 
     public static final String HTML_COMMENTS = "(?s)<!--.*?-->";
     public static final String EMPTY = "";
+    public static final String MAC_OSX = "Mac OS X";
+    public static final String OS_NAME = "os.name";
 
     private final String expectedHtmlResult;
 
@@ -20,8 +27,18 @@ public class HtmlValidatingResultRenderer extends HtmlResultRenderer {
 
     public String render(Result result) throws Exception {
         String actualHtml = super.render(result);
-        assertThat(actualHtml.replaceAll(HTML_COMMENTS, EMPTY)).isEqualToIgnoringWhitespace(expectedHtmlResult.replaceAll(HTML_COMMENTS, EMPTY));
+        if (runningOn(MAC_OSX)) {
+            testExpectation(actualHtml);
+        }
         return actualHtml;
+    }
+
+    public boolean runningOn(String operatingSystem) {
+        return operatingSystem.equalsIgnoreCase(System.getProperty(OS_NAME));
+    }
+
+    public void testExpectation(String actualHtml) {
+        assertThat(actualHtml.replaceAll(HTML_COMMENTS, EMPTY)).isEqualToIgnoringWhitespace(expectedHtmlResult.replaceAll(HTML_COMMENTS, EMPTY));
     }
 
     String loadResource(String name) throws IOException {
