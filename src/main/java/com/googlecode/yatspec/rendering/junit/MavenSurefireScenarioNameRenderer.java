c3ffb42c27043817977770200f4b69b465a5cf4c
nicholas.mcdowall <nicholas.mcdowall@asos.com>
2019-07-04 08:49:47 +0100

    Refactor [remove totallylazy dependency from class]

--- src/main/java/com/googlecode/yatspec/rendering/junit/MavenSurefireScenarioNameRenderer.java
+++ src/main/java/com/googlecode/yatspec/rendering/junit/MavenSurefireScenarioNameRenderer.java
@@ -3,7 +3,7 @@ package com.googlecode.yatspec.rendering.junit;
 import com.googlecode.yatspec.rendering.ScenarioNameRenderer;
 import com.googlecode.yatspec.state.ScenarioName;
 
-import static com.googlecode.totallylazy.Sequences.sequence;
+import static java.util.stream.Collectors.joining;
 
 public class MavenSurefireScenarioNameRenderer implements ScenarioNameRenderer {
 
@@ -12,7 +12,10 @@ public class MavenSurefireScenarioNameRenderer implements ScenarioNameRenderer {
         if (scenarioName.hasEmptyRow()) {
             return scenarioName.getMethodName();
         }
-        
-        return scenarioName.getMethodName() + "__" + sequence(scenarioName.getRow()).toString("_");
+
+        String row = scenarioName.getRow().stream()
+                .collect(joining("_"));
+
+        return scenarioName.getMethodName() + "__" + row;
     }
 }
