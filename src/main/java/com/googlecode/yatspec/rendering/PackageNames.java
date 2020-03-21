573b51f7674d6b19a49ac3375813e3cba77ef462
nickmcdowall <nicholas.mcdowall@gmail.com>
2020-03-21 20:57:01 +0000

    Refactor away more refereces to totallylazy library

--- src/main/java/com/googlecode/yatspec/rendering/PackageNames.java
+++ src/main/java/com/googlecode/yatspec/rendering/PackageNames.java
@@ -1,16 +1,18 @@
 package com.googlecode.yatspec.rendering;
 
-import com.googlecode.totallylazy.*;
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Predicate;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.IntStream;
 
-import static com.googlecode.totallylazy.Callables.returnArgument;
-import static com.googlecode.totallylazy.Sequences.empty;
-import static com.googlecode.totallylazy.Sequences.sequence;
 import static java.lang.String.format;
 import static java.util.regex.Pattern.quote;
+import static java.util.stream.Collectors.toList;
 
 public class PackageNames {
+
     public static Predicate<String> directSubpackageOf(final String parentPackage) {
         return packageName -> {
             String pattern = format("%s\\.?[^.]+$", quote(parentPackage));
@@ -28,34 +30,12 @@ public class PackageNames {
         }
     }
 
-    /*
-    allAncestors().call("com.googlecode.yatspec");
-
-    returns a sequence of:
-
-    "com"
-    "com.googlecode"
-    "com.googlecode.yatspec"
-     */
     public static Callable1<String, Collection<String>> allAncestors() {
-        return new Callable1<>() {
-            @Override
-            public Collection<String> call(String name) {
-                return sequence(name.split("\\.")).
-                        fold(empty(String.class), buildPath());
-            }
-
-            private Callable2<Sequence<String>, String, Sequence<String>> buildPath() {
-                return (allPaths, name) -> {
-                    Option<String> longestPath = allPaths.
-                            sortBy(returnArgument(String.class)).
-                            lastOption();
-                    if (longestPath.isEmpty())
-                        return allPaths.append(name);
-                    else
-                        return allPaths.append(longestPath.get() + "." + name);
-                };
-            }
+        return packageName -> {
+            String[] words = packageName.split("\\.");
+            return IntStream.range(0, words.length)
+                    .mapToObj(index -> String.join(".", Arrays.copyOfRange(words, 0, index + 1)))
+                    .collect(toList());
         };
     }
 }
---
+++ src/test/java/com/googlecode/yatspec/rendering/PackageNamesTest.java
@@ -0,0 +1,44 @@
+package com.googlecode.yatspec.rendering;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PackageNamesTest {
+
+    @Test
+    void allAncestorsSplitsByDots() throws Exception {
+        Collection<String> strings = PackageNames.allAncestors().call("com.googlecode.yatspec");
+
+        assertThat(strings).contains(
+                "com",
+                "com.googlecode",
+                "com.googlecode.yatspec"
+        );
+    }
+
+    @Test
+    void matchesDirectSubpackage() {
+        assertThat(PackageNames.directSubpackageOf("com.googlecode")
+                .matches("com.googlecode.yatspec")).isTrue();
+    }
+
+    @CsvSource(value = {
+            "com.googlecode.yatspec, yatspec",
+            "com.googlecode, googlecode",
+            "com, com"
+    })
+    @ParameterizedTest(name = "'{0}' has a display name of '{1}'")
+    void packageDisplayName(String packageName, String displayName) {
+        assertThat(PackageNames.packageDisplayName(packageName)).isEqualTo(displayName);
+    }
+
+    @Test
+    void emptyPackageDisplaysNameIsForwardSlash() {
+        assertThat(PackageNames.packageDisplayName("")).isEqualTo("/");
+    }
+}
