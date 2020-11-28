bcaf1447e046ad9f84ccb25a512b77dc81fd9eb0
nickmcdowall <nicholas.mcdowall@gmail.com>
2020-11-28 22:49:09 +0000

    Issue#10 Remove totallylazy from JavaSourceRenderer

--- src/main/java/com/googlecode/yatspec/rendering/html/JavaSourceRenderer.java
+++ src/main/java/com/googlecode/yatspec/rendering/html/JavaSourceRenderer.java
@@ -1,28 +1,30 @@
 package com.googlecode.yatspec.rendering.html;
 
-import com.googlecode.totallylazy.Sequence;
 import com.googlecode.yatspec.parsing.JavaSource;
 import com.googlecode.yatspec.parsing.Text;
 import com.googlecode.yatspec.rendering.Renderer;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
-import static com.googlecode.totallylazy.Sequences.sequence;
-import static com.googlecode.totallylazy.Xml.escape;
 import static java.lang.System.lineSeparator;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
+import static org.apache.commons.lang3.StringEscapeUtils.escapeXml11;
 
 public class JavaSourceRenderer implements Renderer<JavaSource> {
     private static final Pattern DOT_CLASS = Pattern.compile("\\.class(\\W|$)");
 
     @Override
     public String render(JavaSource javaSource) {
-        return escape(lines(removeDotClass(javaSource.value().trim()))
+        List<String> lines = lines(removeDotClass(javaSource.value().trim()));
+        return escapeXml11(lines.stream()
                 .map(Text::wordify)
-                .toString("\n"));
+                .collect(joining("\n")));
     }
 
-    public static Sequence<String> lines(final String sourceCode) {
-        return sequence(sourceCode.split(lineSeparator()));
+    public static List<String> lines(final String sourceCode) {
+        return asList(sourceCode.split(lineSeparator()).clone());
     }
 
     public static String removeDotClass(String s) {
--- src/test/java/com/googlecode/yatspec/rendering/html/JavaSourceRendererTest.java
+++ src/test/java/com/googlecode/yatspec/rendering/html/JavaSourceRendererTest.java
@@ -1,23 +1,28 @@
 package com.googlecode.yatspec.rendering.html;
 
 
+import com.googlecode.yatspec.parsing.JavaSource;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static com.googlecode.yatspec.rendering.html.JavaSourceRenderer.removeDotClass;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class JavaSourceRendererTest {
 
+    private final JavaSourceRenderer renderer = new JavaSourceRenderer();
+
     @Test
-    void shouldRemoveDotClassFromRenderedOutput() {
-        assertThat(renderedVersionOf("get(SomeThing.class)"), is("get(SomeThing)"));
-        assertThat(renderedVersionOf("SomeThing.class"), is("SomeThing"));
-        assertThat(renderedVersionOf("get(SomeThing.class).get(SomeThing.class)"), is("get(SomeThing).get(SomeThing)"));
-        assertThat(renderedVersionOf("variable.classButActuallyMethod()"), is("variable.classButActuallyMethod()"));
-        assertThat(renderedVersionOf("variable.class_weird()"), is("variable.class_weird()"));
+    void escapedXmlCharacters() {
+        assertThat(renderer.render(new JavaSource("<abc>&:</abc>\\+:\n"))).isEqualTo("&lt;abc&gt;&amp; &lt;/abc&gt;\\+");
     }
 
-    private String renderedVersionOf(String s) {
-        return JavaSourceRenderer.removeDotClass(s);
+    @Test
+    void shouldRemoveDotClassFromRenderedOutput() {
+        assertThat(removeDotClass("get(SomeThing.class)")).isEqualTo("get(SomeThing)");
+        assertThat(removeDotClass("SomeThing.class")).isEqualTo("SomeThing");
+        assertThat(removeDotClass("get(SomeThing.class).get(SomeThing.class)")).isEqualTo("get(SomeThing).get(SomeThing)");
+        assertThat(removeDotClass("variable.classButActuallyMethod()")).isEqualTo("variable.classButActuallyMethod()");
+        assertThat(removeDotClass("variable.class_weird()")).isEqualTo("variable.class_weird()");
     }
+
 }
