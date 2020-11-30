17cc9e4e9c1e0390cef49cca52ce064c4a4739a3
nickmcdowall <nicholas.mcdowall@gmail.com>
2020-11-30 22:41:30 +0000

    Issue#10 Replace remainder of totallylazy references in HtmlTagIndexRenderer

--- src/main/java/com/googlecode/yatspec/rendering/html/tagindex/HtmlTagIndexRenderer.java
+++ src/main/java/com/googlecode/yatspec/rendering/html/tagindex/HtmlTagIndexRenderer.java
@@ -2,7 +2,6 @@ package com.googlecode.yatspec.rendering.html.tagindex;
 
 import com.googlecode.funclate.Model;
 import com.googlecode.funclate.stringtemplate.EnhancedStringTemplateGroup;
-import com.googlecode.totallylazy.Pair;
 import com.googlecode.yatspec.junit.SpecResultListener;
 import com.googlecode.yatspec.parsing.Files;
 import com.googlecode.yatspec.rendering.Index;
@@ -10,6 +9,7 @@ import com.googlecode.yatspec.rendering.html.HtmlResultRenderer;
 import com.googlecode.yatspec.state.Result;
 import com.googlecode.yatspec.state.TestMethod;
 import org.antlr.stringtemplate.StringTemplate;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.File;
 import java.util.Collection;
@@ -18,10 +18,9 @@ import java.util.Map;
 import java.util.function.Function;
 
 import static com.googlecode.funclate.Model.mutable.model;
-import static com.googlecode.totallylazy.Sequences.repeat;
-import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.yatspec.rendering.html.HtmlResultRenderer.getCssMap;
 import static com.googlecode.yatspec.rendering.html.HtmlResultRenderer.testMethodRelativePath;
+import static java.util.Comparator.comparing;
 import static java.util.Map.Entry.comparingByKey;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
@@ -40,9 +39,9 @@ public class HtmlTagIndexRenderer implements SpecResultListener {
     }
 
     @Override
-    public void complete(File yatspecOutputDir, Result result) {
+    public void complete(File outputDir, Result result) {
         index.add(result);
-        Files.overwrite(outputFile(yatspecOutputDir), render(index));
+        Files.overwrite(outputFile(outputDir), render(index));
     }
 
     public String render(Index index) {
@@ -62,37 +61,39 @@ public class HtmlTagIndexRenderer implements SpecResultListener {
                 .flatMap(Collection::stream)
                 .map(methodTags())
                 .flatMap(Collection::stream)
-                .collect(groupingBy(Pair::first)).entrySet().stream()
+                .collect(groupingBy(Pair::getLeft)).entrySet().stream()
                 .sorted(comparingByKey())
                 .map(toTagModel())
                 .collect(toList());
     }
 
-    private Function<TestMethod, Collection<Pair<String, TestMethod>>> methodTags() {
-        return resultFileAndTestMethod ->
-                sequence(tagFinder.tags(resultFileAndTestMethod))
-                        .zip(repeat(resultFileAndTestMethod));
-    }
-
-    private static Function<Pair<String, TestMethod>, Model> tagModel() {
-        return tagAndTestMethod -> {
-            TestMethod testMethod = tagAndTestMethod.second();
-            return model().
-                    add(TAG_NAME, tagAndTestMethod.first()).
-                    add("package", testMethod.getPackageName()).
-                    add("resultName", testMethod.getName()).
-                    add("url", testMethodRelativePath(testMethod)).
-                    add("class", getCssMap().get(testMethod.getStatus())).
-                    add("name", testMethod.getDisplayName());
-        };
+    private Function<TestMethod, List<Pair<String, TestMethod>>> methodTags() {
+        return testMethod ->
+                tagFinder.tags(testMethod).stream()
+                        .map(tag -> Pair.of(tag, testMethod))
+                        .collect(toList());
     }
 
     private static Function<Map.Entry<String, List<Pair<String, TestMethod>>>, Model> toTagModel() {
         return entry -> model()
                 .add("name", entry.getKey())
                 .add("results", entry.getValue().stream()
+                        .sorted(comparing(pair -> pair.getRight().getName()))
                         .map(tagModel())
                         .collect(toList()));
+    }
+
+    private static Function<Pair<String, TestMethod>, Model> tagModel() {
+        return tagAndTestMethod -> {
+            TestMethod testMethod = tagAndTestMethod.getRight();
+            return model().
+                    add(TAG_NAME, tagAndTestMethod.getLeft()).
+                    add("package", testMethod.getPackageName()).
+                    add("resultName", testMethod.getName()).
+                    add("url", testMethodRelativePath(testMethod)).
+                    add("class", getCssMap().get(testMethod.getStatus())).
+                    add("name", testMethod.getDisplayName());
+        };
     }
 
     private static File outputFile(File outputDirectory) {
