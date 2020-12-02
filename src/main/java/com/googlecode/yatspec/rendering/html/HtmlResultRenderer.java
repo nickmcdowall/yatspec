e9d9c53a45e5199cafaa03d03d09d63937d7441e
nickmcdowall <nicholas.mcdowall@gmail.com>
2020-12-02 22:02:07 +0000

    Issue#10 remove references to totallylazy from HtmlResultRenderer

--- src/main/java/com/googlecode/yatspec/rendering/html/HtmlResultRenderer.java
+++ src/main/java/com/googlecode/yatspec/rendering/html/HtmlResultRenderer.java
@@ -1,8 +1,6 @@
 package com.googlecode.yatspec.rendering.html;
 
 import com.googlecode.funclate.stringtemplate.EnhancedStringTemplateGroup;
-import com.googlecode.totallylazy.Predicate;
-import com.googlecode.totallylazy.Xml;
 import com.googlecode.yatspec.Creator;
 import com.googlecode.yatspec.junit.LinkingNote;
 import com.googlecode.yatspec.junit.Notes;
@@ -16,6 +14,8 @@ import com.googlecode.yatspec.state.Status;
 import com.googlecode.yatspec.state.TestMethod;
 import org.antlr.stringtemplate.NoIndentWriter;
 import org.antlr.stringtemplate.StringTemplate;
+import org.apache.commons.text.StringEscapeUtils;
+import org.jdom.Document;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,11 +23,9 @@ import java.io.StringWriter;
 import java.nio.file.Files;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.*;
+import java.util.function.Predicate;
 
-import static com.googlecode.totallylazy.Callables.asString;
-import static com.googlecode.totallylazy.Predicates.*;
 import static com.googlecode.yatspec.parsing.FilesUtil.overwrite;
-import static com.googlecode.yatspec.rendering.Renderers.registerRenderer;
 import static java.lang.String.format;
 
 public class HtmlResultRenderer implements SpecResultListener {
@@ -47,17 +45,18 @@ public class HtmlResultRenderer implements SpecResultListener {
     public String render(Result result) throws Exception {
         final EnhancedStringTemplateGroup group = new EnhancedStringTemplateGroup(getClass());
         group.setRootDir(null); //forces use of classpath to lookup template
-        group.registerRenderer(always().and(not(instanceOf(Number.class))), Xml.escape());
-        group.registerRenderer(instanceOf(ScenarioTableHeader.class), renderer(new ScenarioTableHeaderRenderer()));
-        group.registerRenderer(instanceOf(JavaSource.class), renderer(new JavaSourceRenderer()));
-        group.registerRenderer(instanceOf(Notes.class), renderer(new NotesRenderer()));
-        group.registerRenderer(instanceOf(LinkingNote.class), renderer(new LinkingNoteRenderer(result.getTestClass())));
-        group.registerRenderer(instanceOf(ContentAtUrl.class), asString());
-        customRenderers.forEach(predicateRendererPair -> registerRenderer().apply(group, predicateRendererPair));
+        group.registerRenderer(o -> !(o instanceof Number), renderer(StringEscapeUtils::escapeXml11));
+        group.registerRenderer(ScenarioTableHeader.class::isInstance, renderer(new ScenarioTableHeaderRenderer()));
+        group.registerRenderer(JavaSource.class::isInstance, renderer(new JavaSourceRenderer()));
+        group.registerRenderer(Notes.class::isInstance, renderer(new NotesRenderer()));
+        group.registerRenderer(LinkingNote.class::isInstance, renderer(new LinkingNoteRenderer(result.getTestClass())));
+        group.registerRenderer(ContentAtUrl.class::isInstance, renderer(Object::toString));
+        customRenderers.forEach(predicateRendererPair ->
+                group.registerRenderer(o -> predicateRendererPair.getKey().test(o), renderer(predicateRendererPair.getValue())));
 
         Optional<Class<?>> optionalDocument = Creator.optionalClass("org.jdom.Document");
         if (optionalDocument.isPresent()) {
-            group.registerRenderer(instanceOf(optionalDocument.get()), renderer(Creator.<Renderer>create(Class.forName("com.googlecode.yatspec.plugin.jdom.DocumentRenderer"))));
+            group.registerRenderer(Document.class::isInstance, renderer(Creator.<Renderer>create(Class.forName("com.googlecode.yatspec.plugin.jdom.DocumentRenderer"))));
         }
 
         final StringTemplate template = group.getInstanceOf("yatspec");
@@ -69,7 +68,7 @@ public class HtmlResultRenderer implements SpecResultListener {
     }
 
     public <T> HtmlResultRenderer withCustomRenderer(Class<T> klazz, Renderer<T> renderer) {
-        return withCustomRenderer(instanceOf(klazz), renderer);
+        return withCustomRenderer(klazz::isInstance, renderer);
     }
 
     private <T> HtmlResultRenderer withCustomRenderer(Predicate<T> predicate, Renderer<T> renderer) {
