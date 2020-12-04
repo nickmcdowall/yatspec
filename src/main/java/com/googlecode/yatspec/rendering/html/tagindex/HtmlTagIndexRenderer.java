15dc9fe36de386644ad2b012ffe4dc1244264fca
nickmcdowall <nicholas.mcdowall@gmail.com>
2020-12-04 21:36:47 +0000

    Issue#13 Replace StringTemplate with JtwigTemplate for tag index page

--- src/main/java/com/googlecode/yatspec/rendering/html/tagindex/HtmlTagIndexRenderer.java
+++ src/main/java/com/googlecode/yatspec/rendering/html/tagindex/HtmlTagIndexRenderer.java
@@ -3,12 +3,11 @@ package com.googlecode.yatspec.rendering.html.tagindex;
 import com.googlecode.yatspec.junit.SpecResultListener;
 import com.googlecode.yatspec.parsing.FilesUtil;
 import com.googlecode.yatspec.rendering.Index;
-import com.googlecode.yatspec.rendering.html.HtmlResultRenderer;
 import com.googlecode.yatspec.state.Result;
 import com.googlecode.yatspec.state.TestMethod;
-import org.antlr.stringtemplate.StringTemplate;
-import org.antlr.stringtemplate.StringTemplateGroup;
 import org.apache.commons.lang3.tuple.Pair;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,7 +17,6 @@ import java.util.Map;
 import java.util.function.Function;
 
 import static com.googlecode.yatspec.rendering.html.HtmlResultRenderer.*;
-import static com.googlecode.yatspec.rendering.html.index.HtmlIndexRenderer.packageUrl;
 import static java.util.Comparator.comparing;
 import static java.util.Map.Entry.comparingByKey;
 import static java.util.stream.Collectors.groupingBy;
@@ -26,8 +24,11 @@ import static java.util.stream.Collectors.toList;
 
 public class HtmlTagIndexRenderer implements SpecResultListener {
     private static final String TAG_NAME = "tag";
-    protected final static Index index = new Index();
+    protected static final Index index = new Index();
+    
     private final TagFinder tagFinder;
+    private final JtwigTemplate template = JtwigTemplate.classpathTemplate("/tagindex_index.twig");
+    private final JtwigModel model = JtwigModel.newModel();
 
     public HtmlTagIndexRenderer() {
         this(new NotesTagFinder());
@@ -46,15 +47,9 @@ public class HtmlTagIndexRenderer implements SpecResultListener {
     }
 
     public String render(Index index) {
-        String packageUrl = packageUrl(getClass()).toString();
-        StringTemplateGroup group = new StringTemplateGroup(packageUrl, packageUrl);
-        group.setRootDir(null); //forces use of classpath to lookup template
-        StringTemplate template = group.getInstanceOf("tagindex_index", Map.of(
-                "stylesheet", HtmlResultRenderer.loadContent("yatspec_alt.css"),
-                "cssClass", getCssMap(),
-                "tags", tagModels(index)));
-
-        return template.toString();
+        return template.render(model
+                .with("cssClass", getCssMap())
+                .with("tags", tagModels(index)));
     }
 
     private List<Map<String, Object>> tagModels(Index index) {
