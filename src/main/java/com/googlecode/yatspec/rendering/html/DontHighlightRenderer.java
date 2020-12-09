bf8c23a3882399518f2db6b4e0543c645634ea74
nickmcdowall <nicholas.mcdowall@gmail.com>
2020-12-09 17:19:28 +0000

    Issue#13 Remove ToStringRenderer references

--- src/main/java/com/googlecode/yatspec/rendering/html/DontHighlightRenderer.java
+++ src/main/java/com/googlecode/yatspec/rendering/html/DontHighlightRenderer.java
@@ -1,12 +1,14 @@
 package com.googlecode.yatspec.rendering.html;
 
 import com.googlecode.yatspec.rendering.Renderer;
-import com.googlecode.yatspec.rendering.ToStringRenderer;
 
 import java.text.MessageFormat;
+import java.util.Optional;
 
 public class DontHighlightRenderer<T> implements Renderer<T> {
     public String render(T instance) {
-        return MessageFormat.format("<div class=''nohighlight''>{0}</div>", new ToStringRenderer().render(instance));
+        return MessageFormat.format("<div class=''nohighlight''>{0}</div>", Optional.ofNullable(instance)
+                .map(Object::toString)
+                .orElse(""));
     }
 }
--- src/test/java/com/googlecode/yatspec/rendering/html/HyperlinkRendererTest.java
+++ src/test/java/com/googlecode/yatspec/rendering/html/HyperlinkRendererTest.java
@@ -1,9 +1,9 @@
 package com.googlecode.yatspec.rendering.html;
 
 import com.googlecode.yatspec.rendering.Renderer;
-import com.googlecode.yatspec.rendering.ToStringRenderer;
 import org.junit.jupiter.api.Test;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -11,45 +11,45 @@ class HyperlinkRendererTest {
     private static final String SINGLE_REFERENCE_URL_FORMAT = "http://myserver.com/browse/%s";
     private static final String MULTIPLE_REFERENCE_URL_FORMAT = "http://myserver.com/browse/%s?id=%s";
     private static final String REGEX = "((?i)bug)-[0-9]{3,5}";
-    private static final Renderer<String> DELEGATE_RENDERER = new ToStringRenderer<>();
+    private static final Renderer<String> DELEGATE_RENDERER = instance -> isEmpty(instance) ? "" : instance;
 
     @Test
-    void shouldNotRenderNullAsHyperlink() throws Exception {
+    void shouldNotRenderNullAsHyperlink() {
         assertRenders(new HyperlinkRenderer<>(SINGLE_REFERENCE_URL_FORMAT, DELEGATE_RENDERER), null, "");
     }
 
     @Test
-    void shouldRenderAnyUrlMatchAsHyperlink() throws Exception {
+    void shouldRenderAnyUrlMatchAsHyperlink() {
         assertRenders(new HyperlinkRenderer<>(SINGLE_REFERENCE_URL_FORMAT, DELEGATE_RENDERER), "BUG-2112", "<a href='http://myserver.com/browse/BUG-2112'>BUG-2112</a>");
         assertRenders(new HyperlinkRenderer<>(SINGLE_REFERENCE_URL_FORMAT, DELEGATE_RENDERER), "My BUG-2112", "<a href='http://myserver.com/browse/My BUG-2112'>My BUG-2112</a>");
     }
 
     @Test
-    void allowsSpecifyingReplacementPattern() throws Exception {
+    void allowsSpecifyingReplacementPattern() {
         assertRenders(new HyperlinkRenderer<>(DELEGATE_RENDERER, "(?:#)([0-9]+)", "<a href='http://myserver.com/$1'>$1</a>"), "text #901 text", "text <a href='http://myserver.com/901'>901</a> text");
     }
 
     @Test
-    void shouldNotRenderNoUrlAsAHyperlink() throws Exception {
+    void shouldNotRenderNoUrlAsAHyperlink() {
         assertRenders(SINGLE_REFERENCE_URL_FORMAT, "nothing", "nothing");
     }
 
     @Test
-    void shouldRenderAUrlMatchAsHyperlink() throws Exception {
+    void shouldRenderAUrlMatchAsHyperlink() {
         assertRenders(SINGLE_REFERENCE_URL_FORMAT, "BUG-2112", "<a href='http://myserver.com/browse/BUG-2112'>BUG-2112</a>");
         assertRenders(SINGLE_REFERENCE_URL_FORMAT, "BUG-2112 - also involved BUG-2312 as well", "<a href='http://myserver.com/browse/BUG-2112'>BUG-2112</a> - also involved <a href='http://myserver.com/browse/BUG-2312'>BUG-2312</a> as well");
     }
 
     @Test
-    void shouldRenderMultipleUrlMatchesAsHyperlink() throws Exception {
+    void shouldRenderMultipleUrlMatchesAsHyperlink() {
         assertRenders(MULTIPLE_REFERENCE_URL_FORMAT, "BUG-2112", "<a href='http://myserver.com/browse/BUG-2112?id=BUG-2112'>BUG-2112</a>");
     }
 
-    private static void assertRenders(String urlFormat, String value, String expected) throws Exception {
+    private static void assertRenders(String urlFormat, String value, String expected) {
         assertRenders(new HyperlinkRenderer<>(urlFormat, REGEX, DELEGATE_RENDERER), value, expected);
     }
 
-    private static void assertRenders(Renderer<String> renderer, String value, String expected) throws Exception {
+    private static void assertRenders(Renderer<String> renderer, String value, String expected) {
         assertThat(renderer.render(value), equalTo(expected));
     }
 }
